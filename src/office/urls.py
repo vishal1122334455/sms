d75da5c5e9ba1fbe3005bc5f53574339346888d2
Mubarak <mubarak117136@gmail.com>
2018-01-31 19:40:03 +0600

    fix some registration system and improve

--- src/office/urls.py
+++ src/office/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.Home.as_view(), name='index'),
+    url(r'^office-registration/$', views.Registration.as_view(), name='office-registration'),
 ]
--- src/office/views.py
+++ src/office/views.py
@@ -10,3 +10,14 @@ class Home(View):
 
     def post(self, request):
         pass
+
+
+#office registration for other official and student, teacher, parent, librarian
+class Registration(View):
+    template_name = 'office/registration.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        pass
