4695ccb7a3b579043214fe96ce404403a29b5f83
Mubarak <mubarak117136@gmail.com>
2018-02-02 17:19:30 +0600

    create model for subjects and class routine

--- src/office/models.py
+++ src/office/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from account import models as mod
+
+
+class ClassRoutine(models.Model):
+    school = models.ForeignKey(mod.School, on_delete=models.CASCADE)
+    classes = models.ForeignKey(mod.Class, on_delete=models.CASCADE)
+    section = models.ForeignKey(mod.Section, on_delete=models.CASCADE)
+    subject = models.ForeignKey(mod.Subject, on_delete=models.CASCADE)
+
+    day = models.CharField(max_length=30, null=True, blank=True)
+    period = models.IntegerField(null=True, blank=True)
+    start_hour = models.TimeField(null=True, blank=True)
+    end_hour = models.TimeField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.day) + "-" + str(self.period)
--- src/office/urls.py
+++ src/office/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
 
     #::::start member operation module url::::
     #:::::::::::::::::::::::::::::::::::::::::
+
     url(r'^office-registration/$', views.Registration.as_view(), name='office-registration'),
 
     #member operations
@@ -23,10 +24,10 @@ urlpatterns = [
     url(r'^member/list/student/class/(?P<classes>[a-zA-Z0-9]+)/section/$', views.ClassWiseSection.as_view(), name='section'),
     url(r'^member/list/student/class/(?P<classes>[a-zA-Z0-9]+)/section/(?P<section>[a-zA-Z0-9-_]+)/$', views.SectionWiseStudent.as_view(), name='section-wise-student'),
 
-
     url(r'^member/detail/(?P<pk>[0-9]+)/$', views.MemberDetail.as_view(), name='member-detail'),
     url(r'^member/edit/(?P<pk>[0-9]+)/$', views.MemberEdit.as_view(), name='member-edit'),
     url(r'^member/delete/(?P<pk>[0-9]+)/$', views.MemberDelete.as_view(), name='member-delete'),
+
     #:::::::::::::::::::::::::::::::::::::::
     #::::end member operation module url::::
 
